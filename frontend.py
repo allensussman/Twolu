b010fa0fdbdba0b23c1e27d1c0026b62231dc0e0
allensussman <allen.sussman@gmail.com>
2014-01-23 18:15:45 -0800

    front end takes up to two movies now

--- frontend.py
+++ frontend.py
@@ -9,15 +9,21 @@ app.debug = True
 
 @app.route('/')
 def my_form():
-    return render_template("index.html",message="",first_movie="",second_movie="")
+    return render_template("index.html",message="",u1m1_val="",u1m2_val="",u2m1_val="",u2m2_val="")
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    movie1 = request.form['movie1']
-    movie2 = request.form['movie2']
-    rec_movies = backend.recommendations([movie1],[movie2])
+    u1m1 = request.form['u1m1_name']
+    u1m2 = request.form['u1m2_name']
+    u2m1 = request.form['u2m1_name']
+    u2m2 = request.form['u2m2_name']
+    u1movies=filter(None,[u1m1,u1m2]) #filter removes empty strings
+    u2movies=filter(None,[u2m1,u2m2]) #filter removes empty strings
+    from code import interact; interact(local=locals())
+
+    rec_movies = backend.recommendations(u1movies,u2movies)
     message="Your ranked recommendations: %s, %s, %s, %s, %s" % tuple(rec_movies)
-    return render_template("index.html",message=message,first_movie=movie1,second_movie=movie2)
+    return render_template("index.html",message=message,u1m1_val=u1m1,u1m2_val=u1m2,u2m1_val=u2m1,u2m2_val=u2m2)
 
 if __name__ == '__main__':
     app.run()
