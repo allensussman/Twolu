61121c2e0aa2227c2388a9022ca1d30d25de5423
allensussman <allen.sussman@gmail.com>
2014-01-23 18:29:52 -0800

    Frontend takes in up to five movies.

--- frontend.py
+++ frontend.py
@@ -9,21 +9,29 @@ app.debug = True
 
 @app.route('/')
 def my_form():
-    return render_template("index.html",message="",u1m1_val="",u1m2_val="",u2m1_val="",u2m2_val="")
+    return render_template("index.html",message="",
+        u1m1_val="",u1m2_val="",u1m3_val="",u1m4_val="",u1m5_val="",u2m1_val="",u2m2_val="",u2m3_val="",u2m4_val="",u2m5_val="")
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     u1m1 = request.form['u1m1_name']
     u1m2 = request.form['u1m2_name']
+    u1m3 = request.form['u1m3_name']
+    u1m4 = request.form['u1m4_name']
+    u1m5 = request.form['u1m5_name']
     u2m1 = request.form['u2m1_name']
     u2m2 = request.form['u2m2_name']
-    u1movies=filter(None,[u1m1,u1m2]) #filter removes empty strings
-    u2movies=filter(None,[u2m1,u2m2]) #filter removes empty strings
+    u2m3 = request.form['u2m3_name']
+    u2m4 = request.form['u2m4_name']
+    u2m5 = request.form['u2m5_name']
+    u1movies=filter(None,[u1m1,u1m2,u1m3,u1m4,u1m5]) #filter removes empty strings
+    u2movies=filter(None,[u2m1,u2m2,u2m3,u2m4,u2m5]) #filter removes empty strings
     from code import interact; interact(local=locals())
 
     rec_movies = backend.recommendations(u1movies,u2movies)
     message="Your ranked recommendations: %s, %s, %s, %s, %s" % tuple(rec_movies)
-    return render_template("index.html",message=message,u1m1_val=u1m1,u1m2_val=u1m2,u2m1_val=u2m1,u2m2_val=u2m2)
+    return render_template("index.html",message=message,
+        u1m1_val=u1m1,u1m2_val=u1m2,u1m3_val=u1m3,u1m4_val=u1m4,u1m5_val=u1m5,u2m1_val=u2m1,u2m2_val=u2m2,u2m3_val=u2m3,u2m4_val=u2m4,u2m5_val=u2m5)
 
 if __name__ == '__main__':
     app.run()
