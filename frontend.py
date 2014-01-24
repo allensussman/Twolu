24ecf8864849349b25d0d085c0c9da5b91665014
allensussman <allen.sussman@gmail.com>
2014-01-23 23:55:40 -0800

    Autocomplete now happens from start of title, but it's kind of slow because it has to do a SQL search.

--- frontend.py
+++ frontend.py
@@ -4,38 +4,39 @@ from flask import render_template
 # flask.json is used to convert Python objects (list/dict) to JSON
 from flask import json
 import backend
+from DbAccess import DbAccess
 
 app = Flask(__name__)
 
 app.debug = True
 
-# # I have this function that returns me a list of cities
-# # This function is called when following address is visited:
-# # /json/cities?q=B
+# I have this function that returns me a list of cmovies
+# This function is called when following address is visited:
+# /json/cities?q=B
+def movies():
+    """Return list of movies where first letters match query."""
 
-# def cities():
-#     """Return list of cities, e.g ["Berlin, Germany", "Bordeaux, France"]."""
-    
+    db = DbAccess('twolu',usr='root')
 
-#     q = request.args.get('q')
-#     # This is my query to find cities, countries matching query
-#     sql = ("SELECT City.name, Country.name "
-#            "FROM City JOIN Country ON City.country_id=Country.id "
-#            "WHERE City.name LIKE '{0}%' OR Country.name LIKE '{0}%'"
-#            "LIMIT 10").format(q)
+    q = request.args.get('q')
 
-#     # Matching cities are in a list
-#     cities = [u'{}, {}'.format(city, country)
-#               for city, country in db(sql)]
-#     # Python list is converted to JSON string
-#     return json.dumps(cities)
+    # This is my query to find movies matching query
+    db.cursor.execute(("SELECT Title FROM movies WHERE Title LIKE '{0}%' LIMIT 10").format(q))
+
+    data = db.cursor.fetchall()
+
+    # Matching movies are in a list
+    movies = [title[0] for title in data]
+
+    # Python list is converted to JSON string
+    return json.dumps(movies)
 
 
-# # I have a dictionary that holds functions
-# # that respond to json requests
-# JSON = {
-#     'cities': cities,
-# }
+# I have a dictionary that holds functions
+# that respond to json requests
+JSON = {
+    'movies': movies,
+}
 
 @app.route('/')
 def my_form():
@@ -61,11 +62,11 @@ def my_form_post():
     return render_template("index.html",message=message,
         u1m1_val=u1m1,u1m2_val=u1m2,u1m3_val=u1m3,u1m4_val=u1m4,u1m5_val=u1m5,u2m1_val=u2m1,u2m2_val=u2m2,u2m3_val=u2m3,u2m4_val=u2m4,u2m5_val=u2m5)
 
-# # `cities` function is called here
-# @app.route("/json/<what>")
-# def ajson(what):
+# `movies` function is called here
+@app.route("/json/<what>")
+def ajson(what):
 
-#     return JSON[what]()
+    return JSON[what]()
 
 if __name__ == '__main__':
     app.run()
